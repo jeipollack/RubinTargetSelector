51e478f81579f9be68708ad5607187c87395c47e
jeipollack <jpollacke@gmail.com>
2022-06-16 16:54:57 +0200

    Starter code for script

---
+++ rubintargets.py
@@ -0,0 +1,16 @@
+import sqlite3
+import pandas as pd
+import numpy as np
+import skycoverage
+import argparse
+import json
+from astropy.time import Time
+
+def get_columns_from_sql(
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Rubin Survey File Generator')
+
+if __name__ == '__main__':
+    pass
